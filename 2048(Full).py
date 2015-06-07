3bce3ecdd950fdfe58e9bb378ee6526ec6de967c
dare <bloody.scooter@gmail.com>
2015-06-07 16:58:18 +0300

    added move method (grid move in 4 directions) and move unit test

--- 2048(Full).py
+++ 2048(Full).py
@@ -21,10 +21,10 @@ RIGHT = 4
 
 # Offsets for computing tile indices in each direction.
 # DO NOT MODIFY this dictionary.
-OFFSETS = {UP: (1, 0),
-           DOWN: (-1, 0),
-           LEFT: (0, 1),
-           RIGHT: (0, -1)}
+OFFSETS = {'UP': (1, 0),
+           'DOWN': (-1, 0),
+           'LEFT': (0, 1),
+           'RIGHT': (0, -1)}
 
 def merge(line):
     """
@@ -84,15 +84,15 @@ class TwentyFortyEight:
         self.grid_height = grid_height
         self.grid_width = grid_width
         self.cells = [[0 for col in range(self.grid_width)] for row in range(self.grid_height)]
-        self.move_dir = {"UP": [], "DOWN": [], "LEFT": [], "RIGHT": []}
+        self.move_dir = {'UP': [], 'DOWN': [], 'LEFT': [], 'RIGHT': []}
         # "UP" =[(0, 0), (0, 1), (0, 2), (0, 3)]
         #self.move_dir["UP"] = "test"
         for col in range(self.grid_width):
-            self.move_dir["UP"].append((0, col))
-            self.move_dir["DOWN"].append((self.grid_height, col))
-        for row in range(self.grid_width):
-            self.move_dir["LEFT"].append((row, 0))
-            self.move_dir["RIGHT"].append((row, self.grid_width))
+            self.move_dir['UP'].append((0, col))
+            self.move_dir['DOWN'].append((self.grid_height - 1, col))
+        for row in range(self.grid_height):
+            self.move_dir['LEFT'].append((row, 0))
+            self.move_dir['RIGHT'].append((row, self.grid_width - 1))
 
     def reset(self):
         """
@@ -127,8 +127,33 @@ class TwentyFortyEight:
         Move all tiles in the given direction and add
         a new tile if any tiles moved.
         """
-        # replace with your code
-        pass
+        if direction in ('UP', 'DOWN'):
+            num_steps = self.grid_height
+        elif direction in ('LEFT', 'RIGHT'):
+            num_steps = self.grid_width
+        temp_lists = []
+        temp_list = []
+        for start_cell in self.move_dir[direction]:
+            # step 1: iterate through each line, write results to temp list
+            for step in range(num_steps):
+                row = start_cell[0] + step * OFFSETS[direction][0]
+                col = start_cell[1] + step * OFFSETS[direction][1]
+                temp_list.append(self.cells[row][col])
+            # step 2: merge temp list
+            temp_list = merge(temp_list)
+            # step 3: store merged temp list back on grid
+            idx = 0
+            for step in range(num_steps):
+                row = start_cell[0] + step * OFFSETS[direction][0]
+                col = start_cell[1] + step * OFFSETS[direction][1]
+                if direction in ('UP', 'DOWN'):
+                    self.cells[row][col] = temp_list[idx]
+                    idx += 1
+                elif direction in ('LEFT', 'RIGHT'):
+                    self.cells[row][col] = temp_list[idx]
+                    idx += 1
+            temp_list = []
+        return self.cells
 
     def new_tile(self):
         """
@@ -173,11 +198,21 @@ if __name__ == "__main__":
     #print("should return [4, 4, 2, 0, 0]",merge([2, 2, 2, 2, 2]))
     #print("should return [8, 32, 8, 0]",merge([8, 16, 16, 8]))
     #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
-    field = TwentyFortyEight(3, 5)
+    field = TwentyFortyEight(2, 3)
     test = poc_simpletest.TestSuite()
-    test.run_test(field.new_tile(), "[[2, 2, 0, 2, 0], [0, 0, 0, 0, 2], [0, 2, 2, 0, 0]]", "test1")
-    test.report_results()
+    #test.run_test(field.new_tile(), "[[2, 2, 0, 2, 0], [0, 0, 0, 0, 2], [0, 2, 2, 0, 0]]", "test1")
+    #test.report_results()
     #field.reset()
-    field.reset()
+    #field.cells = [[1, 2, 3], [4, 5, 6]]
+    field.cells = [[0, 2, 0], [2, 2, 2]]
+    test.run_test(str(field.move('RIGHT')), "[[0, 0, 2], [0, 2, 4]]", "move right")
+    field.cells = [[0, 2, 0], [2, 2, 2]]
+    test.run_test(str(field.move('LEFT')), "[[2, 0, 0], [4, 2, 0]]", "move left")
+    field.cells = [[0, 2, 0], [2, 2, 2]]
+    test.run_test(str(field.move('UP')), "[[2, 4, 2], [0, 0, 0]]", "move up")
+    field.cells = [[0, 2, 0], [2, 2, 2]]
+    test.run_test(str(field.move('DOWN')), "[[0, 0, 0], [2, 4, 2]]", "move down")
+    test.report_results()
     print(field.cells)
-    print(field.move_dir)
+    #print(field.move_dir)
+    field.move('UP')
