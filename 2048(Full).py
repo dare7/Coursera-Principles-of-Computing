629eaac1159f7f9edd08d5b93bfef339e1e95cf6
dare <bloody.scooter@gmail.com>
2015-06-08 14:16:52 +0300

    added gui, clean up code, remade new tile logic

--- 2048(Full).py
+++ 2048(Full).py
@@ -1,6 +1,6 @@
 """
 Clone of 2048 game.
-Online version(python 2.6):
+Online version(python 2.6): http://www.codeskulptor.org/#user40_vUM4HYMcn4Ii7nw_0.py
 """
 __author__ = 'dare7'
 try:
@@ -21,10 +21,10 @@ RIGHT = 4
 
 # Offsets for computing tile indices in each direction.
 # DO NOT MODIFY this dictionary.
-OFFSETS = {'UP': (1, 0),
-           'DOWN': (-1, 0),
-           'LEFT': (0, 1),
-           'RIGHT': (0, -1)}
+OFFSETS = {UP: (1, 0),
+           DOWN: (-1, 0),
+           LEFT: (0, 1),
+           RIGHT: (0, -1)}
 
 def merge(line):
     """
@@ -81,64 +81,68 @@ class TwentyFortyEight:
     """
 
     def __init__(self, grid_height, grid_width):
-        self.grid_height = grid_height
-        self.grid_width = grid_width
-        self.cells = [[0 for col in range(self.grid_width)] for row in range(self.grid_height)]
-        self.move_dir = {'UP': [], 'DOWN': [], 'LEFT': [], 'RIGHT': []}
+        self._grid_height = grid_height
+        self._grid_width = grid_width
+        self._cells = [[0 for col in range(self._grid_width)] for row in range(self._grid_height)]
+        self._move_dir = {UP: [], DOWN: [], LEFT: [], RIGHT: []}
         # "UP" =[(0, 0), (0, 1), (0, 2), (0, 3)]
-        #self.move_dir["UP"] = "test"
-        for col in range(self.grid_width):
-            self.move_dir['UP'].append((0, col))
-            self.move_dir['DOWN'].append((self.grid_height - 1, col))
-        for row in range(self.grid_height):
-            self.move_dir['LEFT'].append((row, 0))
-            self.move_dir['RIGHT'].append((row, self.grid_width - 1))
+        #self._move_dir["UP"] = "test"
+        for col in range(self._grid_width):
+            self._move_dir[UP].append((0, col))
+            self._move_dir[DOWN].append((self._grid_height - 1, col))
+        for row in range(self._grid_height):
+            self._move_dir[LEFT].append((row, 0))
+            self._move_dir[RIGHT].append((row, self._grid_width - 1))
+        self.new_tile()
+        self.new_tile()
+
 
     def reset(self):
         """
         Reset the game so the grid is empty except for two
         initial tiles.
         """
-        self.cells = [[0 for col in range(self.grid_width)] for row in range(self.grid_height)]
+        self._cells = [[0 for col in range(self._grid_width)] for row in range(self._grid_height)]
         self.new_tile()
         self.new_tile()
-        return self.cells
+        #return self._cells
 
     def __str__(self):
         """
         Return a string representation of the grid for debugging.
         """
-        return str(self.cells)
+        return str(self._cells)
 
     def get_grid_height(self):
         """
         Get the height of the board.
         """
-        return self.grid_height
+        return self._grid_height
 
     def get_grid_width(self):
         """
         Get the width of the board.
         """
-        return self.grid_width
+        return self._grid_width
 
     def move(self, direction):
         """
         Move all tiles in the given direction and add
         a new tile if any tiles moved.
         """
-        if direction in ('UP', 'DOWN'):
-            num_steps = self.grid_height
-        elif direction in ('LEFT', 'RIGHT'):
-            num_steps = self.grid_width
-        temp_lists = []
+        if direction in (UP, DOWN):
+            num_steps = self._grid_height
+        elif direction in (LEFT, RIGHT):
+            num_steps = self._grid_width
+        cells_init = []
+        cells_init.append(self._cells)
         temp_list = []
-        for start_cell in self.move_dir[direction]:
+        for start_cell in self._move_dir[direction]:
             # step 1: iterate through each line, write results to temp list
             for step in range(num_steps):
                 row = start_cell[0] + step * OFFSETS[direction][0]
                 col = start_cell[1] + step * OFFSETS[direction][1]
-                temp_list.append(self.cells[row][col])
+                temp_list.append(self._cells[row][col])
             # step 2: merge temp list
             temp_list = merge(temp_list)
             # step 3: store merged temp list back on grid
@@ -146,14 +150,18 @@ class TwentyFortyEight:
             for step in range(num_steps):
                 row = start_cell[0] + step * OFFSETS[direction][0]
                 col = start_cell[1] + step * OFFSETS[direction][1]
-                if direction in ('UP', 'DOWN'):
-                    self.cells[row][col] = temp_list[idx]
+                if direction in (UP, DOWN):
+                    self._cells[row][col] = temp_list[idx]
                     idx += 1
-                elif direction in ('LEFT', 'RIGHT'):
-                    self.cells[row][col] = temp_list[idx]
+                elif direction in (LEFT, RIGHT):
+                    self._cells[row][col] = temp_list[idx]
                     idx += 1
             temp_list = []
-        return self.cells
+        if cells_init != self._cells:
+            self.new_tile()
+        score = sum(map(sum, self._cells))
+        print("Your score: %s" % score)
+        #return self._cells
 
     def new_tile(self):
         """
@@ -161,58 +169,57 @@ class TwentyFortyEight:
         square.  The tile should be 2 90% of the time and
         4 10% of the time.
         """
-        #self.cells = [[0 for col in range(self.grid_width)] for row in range(self.grid_height)]
-        col = random.randrange(0,self.grid_width)
-        row = random.randrange(0,self.grid_height)
-        chance = random.randrange(0,10)
-        if any(0 in sublist for sublist in self.cells):
-            if self.cells[row][col] == 0:
-                if chance == 9:
-                    self.cells[row][col] = 4
-                else:
-                    self.cells[row][col] = 2
+        zero_list = []
+        zero_cell = ()
+        # self._cells = [[0 for col in range(self._grid_width)] for row in range(self._grid_height)]
+        for row in range(self._grid_height):
+            for col in range(self._grid_width):
+                if self._cells[row][col] == 0:
+                    zero_cell = (row, col)
+                    zero_list.append(zero_cell)
+        if len(zero_list) > 0:
+            chance = random.randrange(0,10)
+            cell_idx = random.randrange(len(zero_list))
+            if chance == 9:
+                self._cells[zero_list[cell_idx][0]][zero_list[cell_idx][1]] = 4
             else:
-                col = random.randrange(0,self.grid_width)
-                row = random.randrange(0,self.grid_height)
+                self._cells[zero_list[cell_idx][0]][zero_list[cell_idx][1]] = 2
         else:
-            print("no empty tiles")
-        return self.cells
+            print("You lost! Better luck next time!")
 
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
         """
-        self.cells[row][col] = value
+        self._cells[row][col] = value
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """
-        return self.cells[row][col]
+        return self._cells[row][col]
+
 
 if __name__ == "__main__":
     # tests
+    # merge unit tests
     #print("should return [4, 4, 0, 0]",merge([2, 0, 2, 4]))
     #print("should return [4, 0, 0, 0]",merge([0, 0, 2, 2]))
     #print("should return [4, 0, 0, 0]",merge([2, 2, 0, 0]))
     #print("should return [4, 4, 2, 0, 0]",merge([2, 2, 2, 2, 2]))
     #print("should return [8, 32, 8, 0]",merge([8, 16, 16, 8]))
+    poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
+
     #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
-    field = TwentyFortyEight(2, 3)
-    test = poc_simpletest.TestSuite()
-    #test.run_test(field.new_tile(), "[[2, 2, 0, 2, 0], [0, 0, 0, 0, 2], [0, 2, 2, 0, 0]]", "test1")
+    #field = TwentyFortyEight(2, 3)
+    #test = poc_simpletest.TestSuite()
+    # move unit test
+    #field._cells = [[0, 2, 0], [2, 2, 2]]
+    #test.run_test(str(field.move(RIGHT)), "[[0, 0, 2], [0, 2, 4]]", "move right")
+    #field._cells = [[0, 2, 0], [2, 2, 2]]
+    #test.run_test(str(field.move('LEFT')), "[[2, 0, 0], [4, 2, 0]]", "move left")
+    #field._cells = [[0, 2, 0], [2, 2, 2]]
+    #test.run_test(str(field.move('UP')), "[[2, 4, 2], [0, 0, 0]]", "move up")
+    #field._cells = [[0, 2, 0], [2, 2, 2]]
+    #test.run_test(str(field.move('DOWN')), "[[0, 0, 0], [2, 4, 2]]", "move down")
     #test.report_results()
-    #field.reset()
-    #field.cells = [[1, 2, 3], [4, 5, 6]]
-    field.cells = [[0, 2, 0], [2, 2, 2]]
-    test.run_test(str(field.move('RIGHT')), "[[0, 0, 2], [0, 2, 4]]", "move right")
-    field.cells = [[0, 2, 0], [2, 2, 2]]
-    test.run_test(str(field.move('LEFT')), "[[2, 0, 0], [4, 2, 0]]", "move left")
-    field.cells = [[0, 2, 0], [2, 2, 2]]
-    test.run_test(str(field.move('UP')), "[[2, 4, 2], [0, 0, 0]]", "move up")
-    field.cells = [[0, 2, 0], [2, 2, 2]]
-    test.run_test(str(field.move('DOWN')), "[[0, 0, 0], [2, 4, 2]]", "move down")
-    test.report_results()
-    print(field.cells)
-    #print(field.move_dir)
-    field.move('UP')
